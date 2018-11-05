63bddf94bafea0752ffc7d0cf2981948e69972b4
doquangduy <do.quang.duy@framgia.com>
2018-11-05 20:37:53 +0700

    init model

---
+++ src/main/java/com/gpch/login/model/RestaurantMaster.java
@@ -0,0 +1,22 @@
+package com.gpch.login.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "RetaurantMaster")
+public class RestaurantMaster {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+}
